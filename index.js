32c19df529f937454993b57d64e390e3ccd00aec
Wandile Nhlapo <wandile.intern@bitcube.tech>
2025-09-05 12:57:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
   sections.forEach(section => {
     section.classList.add("zoom-effect");
   });
+// Toggle Dark/Light Mode
+const toggleModeBtn = document.getElementById("toggleMode");
+toggleModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleModeBtn.textContent = "Light Mode";
+  } else {
+    toggleModeBtn.textContent = "Dark Mode";
+  }
+});
